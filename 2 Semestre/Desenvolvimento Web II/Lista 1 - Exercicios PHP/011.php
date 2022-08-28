5e4329707d206d94a24f378aaf546d61cf867b95
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-08-28 15:31:33 -0300

    Add files via upload

---
+++ 2 Semestre/Desenvolvimento Web II/Lista 1 - Exercicios PHP/011.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Faça o cálculo da média simples de 
+$nota_1 e $nota_2.
+Imprima a média simples.
+Imprima a condição:
+    Se a média menor ou igual a 4.9, reprovado.
+    Se a média entre 5 e 7, recuperação.
+    Se a média acima de 7, aprovado.
+
+Crie uma função com o nome calculo_media.
+Use tipagem estrita e limite os parâmetros a receber float.
+*/
+
+function calculo_media(float $nota_1, float $nota_2): float{
+    $soma = $nota_1 + $nota_2;
+    $media = $soma / 2;
+
+    if($media <= 4.9){
+        echo 'Aluno reprovado' . '<br>' . PHP_EOL;
+    }else if($media >= 5 && $media <=7){
+        echo 'Aluno de recuperação' . '<br>' . PHP_EOL;
+    }else if($media > 7){
+     echo 'Aluno apvovado' . '<br>' . PHP_EOL;
+}
+
+    echo 'A média de ' . $nota_1 . ' e ' . $nota_2 . ' é: ' . $media . '<br>' . PHP_EOL; 
+
+    return 0.0;
+}
+$nota_1 = 8;
+$nota_2 = 9;
+
+$media = calculo_media($nota_1, $nota_2);
+
+
+
+?>
