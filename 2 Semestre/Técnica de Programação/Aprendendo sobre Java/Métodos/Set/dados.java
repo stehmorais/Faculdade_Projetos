f100653aaa9e5de6b1b81034fcc15e8db78f334e
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-09-05 12:13:37 -0300

    método set

---
+++ 2 Semestre/Técnica de Programação/Aprendendo sobre Java/Métodos/Set/dados.java
@@ -0,0 +1,19 @@
+public void setPortas(int portas){
+        this.portas = portas;
+    }
+		
+		public void setRodas(int rodas){
+        this.rodas = rodas;
+    }
+    
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+    
+    public void setGasolina(boolean gasolina){
+        this.gasolina = gasolina;
+    }
