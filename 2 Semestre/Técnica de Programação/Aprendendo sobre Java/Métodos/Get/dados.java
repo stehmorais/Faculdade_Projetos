92dac8b8efa47c5d4d6970db5f749a2adc7e6afa
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-09-05 12:12:32 -0300

    método get

---
+++ 2 Semestre/Técnica de Programação/Aprendendo sobre Java/Métodos/Get/dados.java
@@ -0,0 +1,17 @@
+// métodos getters e setters
+//get
+    public int getRodas(){
+        return rodas;
+    }
+    
+    public int getPortas(){
+        return portas;
+    }
+    
+    public String getPlaca(){
+        return placa;
+    }
+    
+    public String getCor(){
+        return cor;
+    }
