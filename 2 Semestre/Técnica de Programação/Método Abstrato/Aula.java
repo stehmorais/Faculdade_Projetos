b7357f0475309adff02787c627a4f03c815dc715
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-10-07 22:18:42 -0300

    Create Aula.java

---
+++ 2 Semestre/Técnica de Programação/Método Abstrato/Aula.java
@@ -0,0 +1,12 @@
+public class Aula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        ExtendsAbstract e = new ExtendsAbstract();
+        e. // vai mostrar os textos abstratos
+    }
+    
+}
