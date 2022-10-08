aa42c6cf0df00b52956e5fbbf4c37a768024f666
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-10-07 22:17:07 -0300

    Create minhaClasseAbstrata.java

---
+++ 2 Semestre/Técnica de Programação/Método Abstrato/minhaClasseAbstrata.java
@@ -0,0 +1,15 @@
+package aulapolimorfismo;
+
+/**
+ *
+ * @author aluno.si
+ */
+public abstract class minhaClasseAbstrata {
+    
+    public abstract void texto();
+    
+    public abstract void texto1();
+    
+    public abstract int texto2();
+    
+}
