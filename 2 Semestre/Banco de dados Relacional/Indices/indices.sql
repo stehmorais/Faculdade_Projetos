bf5c497c377d9d08ad309b80935439c97d7c46f0
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-11-17 22:10:46 -0300

    Update indices.sql

--- 2 Semestre/Banco de dados Relacional/Indices/indices.sql
+++ 2 Semestre/Banco de dados Relacional/Indices/indices.sql
@@ -1,3 +1,4 @@
+/* indice unico */
 CREATE INDEX numero ON agenda_indexada (numero(4))
 
 SELECT SQL_NO_CACHE * 
@@ -7,4 +8,14 @@ WHERE ddd=19 AND numero= 6287;
 SELECT SQL_NO_CACHE * 
 FROM agenda_indexada 
 WHERE ddd=19 AND numero= 6287;
+
+/* composto */
+CREATE INDEX idx_cliente
+ON cliente(nome,tipo)
+
+/* index com chave unica */
+CREATE UNIQUE INDEX idx_produto
+ON produto (descricao)
+
+
 
