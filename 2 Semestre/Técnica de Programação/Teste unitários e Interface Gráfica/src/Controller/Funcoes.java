81987bf373a9b73d125b65860beb8505f18d77cf
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-11-18 22:19:18 -0300

    Create Funcoes.java

---
+++ 2 Semestre/Técnica de Programação/Teste unitários e Interface Gráfica/src/Controller/Funcoes.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author aluno.si
+ */
+public class Funcoes {
+    public int somar(int x, int y){
+        int resultado = 0;
+        
+        resultado = x + y;
+        return resultado;
+    }
+}
