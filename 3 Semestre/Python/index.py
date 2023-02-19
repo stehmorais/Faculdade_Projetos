d19d5af01d4c185650ca8d54135a9b3ddc7ccc2b
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2023-02-19 14:16:29 -0300

    Create index.py

---
+++ 3 Semestre/Python/index.py
@@ -0,0 +1,112 @@
+import keyword
+print(keyword.kwlist)
+
+print("Primeiro comando")
+print("Segundo comando")
+print("Terceiro comando")
+
+condicao = False 
+
+if condicao:
+    print("passou por aqui")
+
+condicao = True
+
+if condicao:
+    print("Passou no if falso")
+
+#for
+
+a = [1,2,3,4,5,6,7,8,9,10]
+
+for numero in a:
+    print(numero)
+
+#funcao
+def caixinha_magica(numero1, numero2):
+    numero_magico = numero1 + numero2
+    return numero_magico
+
+print(caixinha_magica(2,5))
+
+print(caixinha_magica('oi','mundo'))
+
+
+def caixinha_magica2(parametro1, parametro2):
+    retorno_magico = parametro1 - parametro2
+    return retorno_magico
+
+assert caixinha_magica2(10,4) == 6
+assert caixinha_magica2(10,5) == 5
+
+nome1 = 'fatec araras'
+print(id(nome1))
+
+lista = [] #criando lista
+
+lista.append('Pão') #adicionando objeto a lista
+lista.append('Bacon')
+lista.append(2)
+
+print(lista)
+
+for comida in lista: #for para procurar na lista
+    print(comida)
+
+lista2 = []
+lista2.append('Chocolate')
+lista2.append('Banana')
+lista.append('Mamão')
+
+lista2.pop() #remove o ultimo objeto da lista
+lista.remove('Mamão') #remove objeto especifico
+
+for comida in lista2:
+    print(comida.upper()) #imprime tudo em capslock
+
+#Tuplas - N é para mexer, geralmente imutáveis
+
+tupla1 = ('Pão', 'Bacon', 'Chocolate', 'Chocolate')
+print (tupla1)
+
+tupla1.count('Chocolate')
+tupla1.index('Bacon')
+
+print(tupla1.count('Chocolate'))
+print(tupla1.index('Bacon'))
+
+#transformando tupla em lista 
+
+lista3 = list(tupla1) 
+print(lista3)
+
+#transformando lista em tupla
+x = tuple(lista)
+
+print(x)
+
+lista4 = []
+
+for item in lista:
+    if type (item) == str:
+        lista4.append(item)
+print(lista4)
+
+#range
+
+r = range(1,15)
+print(list(r))
+
+r = range(1,15,2)
+print(list(r))
+
+#desafio
+
+assert caixinha_magica3(20, 15) == 5
+assert caixinha_magica3(20, 10) == 10
+assert caixinha_magica3(10, 10) == 0
+assert caixinha_magica3(10, 15) == 0
+assert caixinha_magica3(30, 45) == 0
+
+print(caixinha_magica3(50 , 30))
+print(caixinha_magica3(20 , 30))
